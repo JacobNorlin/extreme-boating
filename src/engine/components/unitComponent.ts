dac21ccbd3d01e78989393f37aba180234149b06
Jacob Norlin Andersson <jacob.norlinandersson@gmail.com>
2023-02-09 17:40:54 +0100

    Update UnitComponent to use abstract class

--- src/engine/components/unitComponent.ts
+++ src/engine/components/unitComponent.ts
@@ -1,12 +1,13 @@
 import { Unit } from "w3ts";
 import { Component } from "../ecs/component";
 
-export class UnitComponent implements Component<"unit"> {
+export class UnitComponent extends Component<"unit"> {
     type: "unit" = "unit";
 
     unit: Unit;
 
     constructor(unit: Unit) {
+        super();
         this.unit = unit;
     }
 
